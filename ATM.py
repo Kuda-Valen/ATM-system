feee8efae791f4b75586a573332f0d09c63f0fd4
Kuda_Valen <kudamukwasi@gmail.com>
2026-06-11 14:52:37 +0200

    first commit

---
+++ ATM.py
@@ -0,0 +1,43 @@
+# ATM Machine system
+"""
+Features: 
+PIN Authentication
+Check Balance
+Deposit Money
+Withdraw Money
+Change PIN
+Mini Statement
+"""
+
+import datetime
+import pandas as pd
+
+users = []
+account_numbers = []
+default_acc = 100000
+
+class User():
+    def __init__(self, name: str, email: str, number: int):
+        self. name = name
+        self.email = email
+        self.number = number
+
+    def accountNumber(self): 
+        account_numbers.append(default_acc)
+
+        last_account = account_numbers[-1]
+        account_number += last_account
+        return 
+    
+    def add_user(self):
+        name = input("Enter user name: ")
+        email  = input("Enter email: ")
+        number = int(input("Enter phone number: "))
+        balance = 0.0
+    
+    def append(self): 
+        
+
+
+
+
