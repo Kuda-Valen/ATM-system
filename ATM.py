0b51b8a089faf3ad5f82671326fec41bc8047a70
Kuda_Valen <kudamukwasi@gmail.com>
2026-06-30 11:29:18 +0200

    Save all

--- ATM.py
+++ ATM.py
@@ -10,17 +10,16 @@ Mini Statement
 """
 
 import datetime
-from Bank import  createAcc, update_df, print_users, deposit, withdraw, transfer, userSettings, log_in, forgot_pin
+from Bank import  update_df, log_in, forgot_pin
 
 if __name__ == "__main__":
 
     while True: 
-        print("ATM Machine")
+        print("\nATM Machine")
         print("1. Create an Account")
         print("2. Log In")
         print("3. Forgot Pin")
-        print("4. Views users")
-        print("7. Exit")
+        print("5. Exit")
 
         try: 
             option = int(input("\nChoose an option: "))
@@ -34,10 +33,10 @@ if __name__ == "__main__":
             elif option == 3:
                 forgot_pin()
             
-            elif option == 4:
-                print_users()
+            #elif option == 4:
+                #print_users()
             
-            elif option == 7:
+            elif option == 5:
                 print("Exiting...")
                 break
 
